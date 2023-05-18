07ed4ab730c64b814eb4aebf08a7226cb4a34564
Finley <hongfeiz@mun.ca>
2023-05-18 13:18:04 -0300

    automate postgres restart & prisma migration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,7 +9,7 @@ export class AuthController {
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
-  @Post('login')
+  @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.login(dto);
   }
